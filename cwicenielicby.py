8ea9e3e13cc6a5839d7cd6aebc81f839f1cd4ff2
Raldaly <110200140+Raldaly@users.noreply.github.com>
2022-08-04 18:09:56 +0200

    Add files via upload

---
+++ cwicenielicby.py
@@ -0,0 +1,9 @@
+print('numbers divisible by 7, not divisible by 5, from 1 to 470')
+print('\n')
+
+numbers= [
+    print(number, '/7 = ', int(number/7))
+    for number in range(2, 471)
+    if (number % 7 == 0)
+    if (number % 5 != 0)
+    ]
